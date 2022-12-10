df85237a2d32ee10aba07c2ffd516065b51ced3c
mikolaj semeniuk <m.semeniuk@icloud.com>
2022-12-10 16:16:58 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import "../styles/output.css";
+
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="m-12 text-4xl text-red-600">
+      Welcome to NextJS 13 with tailwindcss and turbopack
+    </h1>
+  );
+}
