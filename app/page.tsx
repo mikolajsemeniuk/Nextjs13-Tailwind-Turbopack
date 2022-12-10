ec865c805b7e59e423269f9e34b161e9f3a09c57
mikolaj semeniuk <m.semeniuk@icloud.com>
2022-12-10 16:17:53 +0100

    _

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="m-12 text-4xl text-red-600">
-      Welcome to NextJS 13 with tailwindcss and turbopack
-    </h1>
+    <>
+      <h1 className="m-12 text-4xl text-red-600">
+        Welcome to NextJS 13 with tailwindcss and turbopack
+      </h1>
+      <p>Hi</p>
+    </>
   );
 }
